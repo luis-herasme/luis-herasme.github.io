8e314c217e1cf59be5aa738edeada0f8062f1289
LuisHerasme <luis.alberto.herasme.cuevas@gmail.com>
2019-01-28 16:03:11 -0400

    Fractal fix

--- Fractales/index.js
+++ Fractales/index.js
@@ -1,158 +1,343 @@
-/**
- *
- * TODO
- *
- * Add to options:
- *  Canvas resolution.
- *  Colors.
- *  N, Iterations
- *  N, Infinity
- *  Zoom
- */
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
 
-import Complex from "./Complex";
-import { hslToRgb } from "./Color";
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
 
-const canvasWidth = 500;
-const canvasHeight = 500;
-const c = new Complex(-1, -0.3); //new Complex(-0.1, -0.3);
-let n, z, col;
-let counter = 0;
-let N_ITERATION = 100;
-let N_MAX = 2;
-
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
-
-document.getElementById("cambiar").addEventListener("click", () => {
-  c.Re = Number(document.getElementById("real").value);
-  c.Im = Number(document.getElementById("im").value);
-});
-
-var canvasData = context.getImageData(0, 0, canvasWidth, canvasHeight);
-
-var index;
-function drawPixel(x, y, r, g, b, a) {
-  index = (x + y * canvasWidth) * 4;
-  canvasData.data[index + 0] = r;
-  canvasData.data[index + 1] = g;
-  canvasData.data[index + 2] = b;
-  canvasData.data[index + 3] = a;
-}
-
-function set_pixel(data, w, x, y, r, g, b, a) {
-  index = (x + y * w) * 4;
-  data.data[index + 0] = r;
-  data.data[index + 1] = g;
-  data.data[index + 2] = b;
-  data.data[index + 3] = a;
-}
-
-window.generateFractal = generateFractal;
-window.context = context;
-
-function generateFractal(w, h, scale) {
-  const temp_canvas = document.createElement("canvas");
-  const temp_context = temp_canvas.getContext("2d");
-  temp_canvas.width = w;
-  temp_canvas.height = h;
-  let temp_data = temp_context.getImageData(0, 0, w, h);
-  temp_data = fractal(w, h, scale, temp_data);
-  /*
-  for (let x = 0; x < w; x++) {
-    for (let y = 0; y < h; y++) {
-      z = new Complex(scale * (x / w) - scale / 2, scale * (y / h) - scale / 2); // Zoom.
-      n = 0;
-
-      while (n < N_ITERATION) {
-        z.add(z.getSquare());
-        z.add(c);
-
-        if (Math.hypot(z.Re, z.Im) > N_MAX) {
-          break;
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
         }
 
-        n++;
-      }
-
-      n = n / N_ITERATION + 0.0; // Color.
-      col = hslToRgb(n, 1, 0.5);
-      set_pixel(temp_data, w, x, y, col[0], col[1], col[2], n * 255);
-    }
-  }
-  */
-  return temp_data;
-}
-
-function fractal(width, height, scale, data) {
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      z = new Complex(
-        scale * (x / width) - scale / 2,
-        scale * (y / height) - scale / 2
-      );
-      n = 0;
-      while (n < N_ITERATION) {
-        z.add(z.getSquare());
-        z.add(c);
-        if (Math.abs(z.Re + z.Im) > N_MAX) break;
-        n++;
-      }
-      n = n / N_ITERATION + 0.0; // Color.
-      col = hslToRgb(n, 1, 0.5);
-      set_pixel(data, width, x, y, col[0], col[1], col[2], n * 255);
-    }
-  }
-  return data;
-}
-
-window.imagedata_to_image = imagedata_to_image;
-function imagedata_to_image(imagedata) {
-  var canvas = document.createElement("canvas");
-  var ctx = canvas.getContext("2d");
-  canvas.width = imagedata.width;
-  canvas.height = imagedata.height;
-  ctx.putImageData(imagedata, 0, 0);
-
-  var image = new Image();
-  image.src = canvas.toDataURL();
-  return image;
-}
-
-function get_img(w, h, scale) {
-  let i = generateFractal(w, h, scale);
-  let img = imagedata_to_image(i);
-  document.body.appendChild(img);
-}
-window.get_img = get_img;
-
-setInterval(() => {
-  c.Im += Math.cos(counter) / 100;
-  c.Re += Math.sin(counter) / 100;
-  counter += 0.05;
-  context.putImageData(canvasData, 0, 0);
-  canvasData = fractal(canvasWidth, canvasHeight, 2, canvasData);
-  /*
-  for (let x = 0; x < canvasWidth; x++) {
-    for (let y = 0; y < canvasHeight; y++) {
-      z = new Complex(
-        3 * (x / canvasWidth) - 1.5,
-        3 * (y / canvasHeight) - 1.5
-      );
-      n = 0;
-      while (n < N_ITERATION) {
-        z.add(z.getSquare());
-        z.add(c);
-        if (Math.abs(z.Re + z.Im) > N_MAX) {
-          break;
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
         }
-        n++;
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
       }
-      n = n / N_ITERATION + 0.0;
-      col = hslToRgb(n, 1, 0.5);
-      drawPixel(x, y, col[0], col[1], col[2], n * 255);
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
     }
-  }  */
-}, 1000 / 10);
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"../../AppData/Roaming/npm/node_modules/parcel/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../../AppData/Roaming/npm/node_modules/parcel/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../../AppData/Roaming/npm/node_modules/parcel/src/builtins/bundle-url.js"}],"../../AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49877" + '/');
+
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      console.clear();
+      data.assets.forEach(function (asset) {
+        hmrApply(global.parcelRequire, asset);
+      });
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.parcelRequire, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAccept(global.parcelRequire, id);
+  });
+}
+},{}]},{},["../../AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
