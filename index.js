659e147b1276dfd23050d161205bb7e6fdef8ee8
LuisHerasme <luis.alberto.herasme.cuevas@gmail.com>
2020-09-18 16:46:35 -0400

    Fix fractals webgl

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const lista = document.getElementById("listaDeProyectos");
 
 const proyectos = [
   {
-    link: "https://github.com/LuisHerasme/Fractals",
+    link: "https://github.com/LuisHerasme/fractalsWGL/index.html",
     img: "./img/fractalWebGl.png",
     name: "Fractales WebGL"
   },
